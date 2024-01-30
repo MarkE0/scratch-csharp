1447ba17b32dc94c969ec1ce53e1ced40b831c53
Mark Phillips <mark_phillips85@hotmail.com>
2024-01-30 18:29:15 +0000

    [MSLearn C#] Another module complete

---
+++ 20240129_MSLearn_TestProject02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Unit 3/8
+// Random dice = new Random();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+// int result = Random.Next();
+
+
+// Unit 4 / 8
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+
+// Unit 5 / 8
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
