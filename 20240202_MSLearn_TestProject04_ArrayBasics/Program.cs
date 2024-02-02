c251c708d61ed66e90f36388627b2b3cfb35235b
Mark Phillips <mark_phillips85@hotmail.com>
2024-02-02 17:14:59 +0000

    [MSLearn C#] Array Basics module

---
+++ 20240202_MSLearn_TestProject04_ArrayBasics/Program.cs
@@ -0,0 +1,48 @@
+﻿//// Unit 2 / 7
+///*
+//string[] fraudulentOrderIDs = new string[3];
+
+//fraudulentOrderIDs[0] = "A123";
+//fraudulentOrderIDs[1] = "B456";
+//fraudulentOrderIDs[2] = "C789";
+////fraudulentOrderIDs[3] = "D000";
+//*/
+
+//string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+//Console.WriteLine($"First:  {fraudulentOrderIDs[0]}");
+//Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+//Console.WriteLine($"Third:  {fraudulentOrderIDs[2]}");
+
+//fraudulentOrderIDs[0] = "F001";
+
+//Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+//Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+
+
+//// Unit 3 / 7 - Implementing the foreach statement
+//int[] inventory = { 200, 450, 700, 175, 250 };
+//int sum = 0;
+//int bin = 0;
+//foreach (int items in inventory)
+//{
+//    sum += items;
+//    bin++;
+//    Console.WriteLine($"Bin {bin} = {items} items (running total: {sum})");
+//}
+//Console.WriteLine($"We have {sum} items in inventory.");
+
+
+
+// Unit 4 / 7 - Complete a challenge activity for nested iteration and selection statements
+string[] orderIDs = { "B123","C234","A345","C15","B177","G3003","C235","B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
